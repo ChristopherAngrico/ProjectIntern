85cd1749601f1389f812b1106b0011f96b8e6da9
ChristopherAngrico <cenlung82@gmail.com>
2023-10-27 18:56:26 +0700

    Change Spawntree script

--- Assets/Script/SpawnTree.cs
+++ Assets/Script/SpawnTree.cs
@@ -5,35 +5,25 @@ using UnityEngine;
 public class SpawnTree : MonoBehaviour
 {
     [SerializeField] private GameObject prefabTree;
-    //private GameObject[] prefabDirts;
-    private GameObject prefab;
-    private bool finishedSpawnTree;
-    //private List<Vector3> spawnPosition = new List<Vector3>();
     private void OnEnable()
     {
-        //StartCoroutine(LateStart());
         StartCoroutine(Spawn());
     }
-    //private IEnumerator LateStart()
-    //{
-    //    yield return new WaitForSeconds(0.1f);
-    //prefabDirts = GameObject.FindGameObjectsWithTag("Dirt");
-    //    foreach (GameObject dirt in prefabDirts)
-    //    {
-    //        spawnPosition.Add(dirt.transform.position);
-    //    }
-    //}
     private IEnumerator Spawn()
     {
         while (true)
         {
             yield return new WaitForSeconds(1f);
             GameObject dirt = GameObject.FindGameObjectWithTag("Dirt");
+            //Finished spawn if all dirt have filled
             if (dirt == null)
             {
                 break;
             }
+            //Change tag dirt that has planted to plant tag to avoid
+            //grab previous position has planted
             dirt.tag = "Plant";
+
             Vector3 dirtPosition = dirt.transform.position;
             GameObject clone = Instantiate(prefabTree, dirtPosition, Quaternion.identity);
         }
